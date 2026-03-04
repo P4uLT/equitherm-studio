edddf4215038839d53bf1d6202d8dceb45391f56
Thomas SAMTER <7680607+P4uLT@users.noreply.github.com>
2026-03-04 21:43:13 +0100

    refactor: extract UI concerns from core to web (#4)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,17 +4,11 @@
 // Types
 export type {
   CurveParams,
-  CurveLimits,
-  ParamLimit,
   PIDState,
   PIDResult,
   DeadbandConfig,
-  DefaultPIDParams,
 } from './types';
 
 // Main calculations
 export { computeFlowTemperature } from './curve';
-export { computePID, createPIDState, isInDeadband, getRoomTempActual } from './pid';
-
-// Constants
-export { CURVE_LIMITS, DEFAULT_PID_PARAMS } from './constants';
+export { computePID, isInDeadband } from './pid';
